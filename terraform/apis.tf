4ef55bbb1fc2a7ed6bfb44de36e1fe6b0319b1b6
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2026-08-07 18:07:34 -0400

    Update apis.tf

--- terraform/apis.tf
+++ terraform/apis.tf
@@ -1,13 +1,13 @@
 resource "google_project_service" "required_apis" {
   for_each = toset([
     "artifactregistry.googleapis.com",
+    "cloudbuild.googleapis.com",
     "firestore.googleapis.com",
     "iam.googleapis.com",
     "run.googleapis.com"
   ])
 
   project = local.project_id
-
   service = each.value
 
   disable_on_destroy = false
