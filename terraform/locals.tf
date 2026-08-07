e52a694ae519bbaea17f535caaf43eded659e9b1
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2026-08-07 17:23:57 -0400

    Add locals configuration for app files and image URI

---
+++ terraform/locals.tf
@@ -0,0 +1,34 @@
+locals {
+  app_files = sort(
+    tolist(
+      fileset(
+        "${path.module}/../app",
+        "**"
+      )
+    )
+  )
+
+  source_hash = substr(
+    sha256(
+      join(
+        "",
+        [
+          for file in local.app_files :
+          filesha256(
+            "${path.module}/../app/${file}"
+          )
+        ]
+      )
+    ),
+    0,
+    12
+  )
+
+  image_uri = (
+    "${var.region}-docker.pkg.dev/"
+    "${local.project_id}/"
+    "${var.repository_name}/"
+    "${var.service_name}:"
+    "${local.source_hash}"
+  )
+}
