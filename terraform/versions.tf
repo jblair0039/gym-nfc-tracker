a589a434f9fe59474679c56e7ac8381a31e111bc
jblair0039 <44126541+jblair0039@users.noreply.github.com>
2026-08-07 17:21:42 -0400

    Create versions.tf

---
+++ terraform/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.0, < 8.0"
+    }
+
+    external = {
+      source  = "hashicorp/external"
+      version = ">= 2.3, < 3.0"
+    }
+  }
+}
